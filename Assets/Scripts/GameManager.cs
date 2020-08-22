7ea3de161565f8535941e95097c8dd8c9bd0f4b8
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-22 19:05:47 +0300

    Complete Task

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,149 +1,106 @@
-﻿using System.Collections;
+﻿using Sirenix.OdinInspector;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Сетка кнопок (родитель)")]
     [SerializeField] private Transform grid; // Сетка кнопок
+    [Header("Префаб кнопки")]
     [SerializeField] private GameObject actionButtonPrefab; // Кнопка действия
-
-    [SerializeField] private TMP_Text reactionText, moodText, actionText;
-    private string currentAction;
+    [Header("Аниматор кошки")]
+    public Animator catAnimator;
+    [Header("Текстовые поля для вывода реакции и настроения")]
+    [SerializeField] private TMP_Text reactionText, moodText;
 
     [Header("Действия, настроения и реакции")]
-    public ActionList actionList;
-    /*
-     Action List -> Actions -> Size = количество доступных действий (Поиграть, Накормить и т.д.)
-     Reactions On Action -> Size = 3 ->
-                                           0 - Действие (Поиграть, Накормить и т.д.)
-                                           1 - Реакция (Сидит, Бегает и т.д.)
-                                           2 - Новое настроение (Плохое, Хорошее, Отличное)
-    */
+    [TableList]
+    public List<Actions> ActionsList;
+
+
 
     void Start()
     {
-        CreateActionButtons(grid, actionButtonPrefab, actionList);
+        reactionText.text = "Реакция: ";
+        moodText.text = "Настроение: "
+            + ActionsList[0].Moods[Random.Range(0, ActionsList[0].Moods.Count)].currentMood; // Рандомное начальное настроение
+
+        CreateActionButtons();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    public void CreateActionButtons(Transform _parent, GameObject _prefab, ActionList _actionList)
+    #region CreateActionButtons() - Создание кнопок исходя из доступных действий
+    public void CreateActionButtons()
     {
-        foreach (var a in _actionList.actions)
+        for (int i = 0; i < ActionsList.Count; i++)
         {
-            GameObject gameObject = Instantiate(_prefab, _parent);
-            string _action = a.reactionsOnAction[0];
+            #region Создание кнопки внутри сетки
+            GameObject gameObject = Instantiate(actionButtonPrefab, grid);
+            #endregion
 
+            #region Добавление текста на кнопку
+            string _action = ActionsList[i].Action;
+            gameObject.transform.GetComponentInChildren<TMP_Text>().text = _action;
+            #endregion
+
+            #region Добавление метода в кнопку
+            int indexOfAction = i;
             Button.ButtonClickedEvent e = new Button.ButtonClickedEvent();
             e.AddListener(() =>
             {
-                DoReaction(_action);
+                DoReaction(indexOfAction);
             });
-
             gameObject.GetComponent<Button>().onClick = e;
-
-            gameObject.transform.GetComponentInChildren<TMP_Text>().text = _action;
+            #endregion
         }
     }
+    #endregion
 
-
-    public void DoReaction(string _action)
+    #region DoReaction(int indexOfAction) - Метод, который вызывается при нажатии на кнопку - выводит в текст реакцию и новое настроение и включает аниматор
+    public void DoReaction(int indexOfAction)
     {
-        reactionText.text = _action;
-
-        //#region moodText.text == "Плохое"
-        //if (moodText.text == "Плохое" && _action == "Поиграть")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Плохое" && _action == "Накормить")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Плохое" && _action == "Погладить")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Плохое" && _action == "Дать пинка")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //#endregion
-
-        //#region moodText.text == "Хорошее"
-        //if (moodText.text == "Хорошее" && _action == "Поиграть")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Хорошее" && _action == "Накормить")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Хорошее" && _action == "Погладить")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Хорошее" && _action == "Дать пинка")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //#endregion
-
-        //#region moodText.text == "Отличное"
-        //if (moodText.text == "Отличное" && _action == "Поиграть")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Отличное" && _action == "Накормить")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Отличное" && _action == "Погладить")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //if (moodText.text == "Отличное" && _action == "Дать пинка")
-        //{
-        //    reactionText.text = "";
-        //    moodText.text = "";
-        //}
-        //#endregion
+        List<MoodsWithReaction> currentMoods = ActionsList[indexOfAction].Moods;
+        for (int i = 0; i < currentMoods.Count; i++)
+        {
+            if (moodText.text == "Настроение: " + currentMoods[i].currentMood)
+            {
+                reactionText.text = "Реакция: " + currentMoods[i].reaction;
+                catAnimator.runtimeAnimatorController = currentMoods[i].animatorController;
+                moodText.text = "Настроение: " + currentMoods[i].newMood;
+            }
+        }
     }
-    public void SetMood(string _mood)
-    {
-        moodText.text = _mood;
-    }
+    #endregion
+
 }
 
-
+#region Действия и настроения с реакциями
 [System.Serializable]
-public class Action
+public class Actions
 {
-    public List<string> reactionsOnAction;
-}
+    [VerticalGroup("Действие")]
+    public string Action;
 
+    [VerticalGroup("Настроения и реакции")]
+    public List<MoodsWithReaction> Moods;
+}
+#endregion
+
+#region Текущее и новое настроения, реакция и аниматор к сделанному действию
 [System.Serializable]
-public class ActionList
+public class MoodsWithReaction
 {
-    public List<Action> actions;
+    [Header("Текущее настроение")]
+    public string currentMood;
+    [Header("Реакция")]
+    public string reaction;
+
+    [Header("Анимация")]
+    public RuntimeAnimatorController animatorController;
+
+    [Header("Новое настроение")]
+    public string newMood;
 }
-
-
+#endregion
---
+++ Assets/Scripts/LoadingObject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingObject : MonoBehaviour
+{
+    public TMP_Text PercentText;
+    public Slider slider;
+
+
+    private int _currentPercent;
+    public int CurrentPercent
+    {
+        get => _currentPercent;
+        set
+        {
+            _currentPercent = value;
+            slider.value = value;
+            PercentText.text = value.ToString() + "%";
+        }
+    }
+
+    private void Start()
+    {
+        slider.maxValue = 100;
+        StartCoroutine(LoadScene(1));
+    }
+
+    private IEnumerator LoadScene(int indexOfScene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(indexOfScene);
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            CurrentPercent = (int)(progress * slider.maxValue);
+            Debug.Log(CurrentPercent);
+            yield return null;
+        }
+    }
+
+}
